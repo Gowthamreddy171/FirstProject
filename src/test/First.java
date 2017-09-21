b2b2d6ac5f90c167a983cf67a6eed8a3e581a803
m172466 <m172466@R5078612.mfad.mfroot.org>
2017-09-21 09:59:36 -0500

    First Commit

---
+++ src/test/First.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class First {
+	
+//	static final Logger logger = Logger.getLogger(First.class);
+//    static final String path = "C://Users//M172466//Desktop//Master//Java//LogVerifier//log4j.properties.txt";
+
+	public static void main(String[] args) {
+//		PropertyConfigurator.configure(path);
+//        logger.debug("Sample debug message");
+//        logger.info("Sample info message");
+//        logger.warn("Sample warn message");
+//        logger.error("Sample error message");
+//        logger.fatal("Sample fatal message");
+		
+		int i = 1;
+		switch(i){
+			case 1:
+				System.out.println("case 1");
+				
+				System.out.println("case 1 2nd line");
+				
+				
+				
+			case 2:System.out.println("case 2");
+			
+			default:System.out.println("default");
+		}
+	}
+	
+	
+
+}
