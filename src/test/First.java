c91c1216aaf9faa3b9eb85d5808f3e5993a3b975
m172466 <m172466@R5078612.mfad.mfroot.org>
2017-09-21 10:13:26 -0500

    Second Commit

--- src/test/First.java
+++ src/test/First.java
@@ -1,20 +1,9 @@
 package test;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
-
 public class First {
 	
-//	static final Logger logger = Logger.getLogger(First.class);
-//    static final String path = "C://Users//M172466//Desktop//Master//Java//LogVerifier//log4j.properties.txt";
 
 	public static void main(String[] args) {
-//		PropertyConfigurator.configure(path);
-//        logger.debug("Sample debug message");
-//        logger.info("Sample info message");
-//        logger.warn("Sample warn message");
-//        logger.error("Sample error message");
-//        logger.fatal("Sample fatal message");
 		
 		int i = 1;
 		switch(i){
