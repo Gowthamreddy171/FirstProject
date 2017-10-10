0fd0490971d7e4a126b8b93c08796043b5f77398
m172466 <m172466@R5078612.mfad.mfroot.org>
2017-10-10 09:20:03 -0500

    testing strings

--- src/test/First.java
+++ src/test/First.java
@@ -4,20 +4,25 @@ public class First {
 	
 
 	public static void main(String[] args) {
-		
-		int i = 1;
-		switch(i){
-			case 1:
-				System.out.println("case 1");
-				
-				System.out.println("case 1 2nd line");
-				
-				
-				
-			case 2:System.out.println("case 2");
-			
-			default:System.out.println("default");
-		}
+		String s1 = "Error";
+		String s2 = "ERROR";
+		if(s1==s2)
+			System.out.println("true");
+		else
+			System.out.println("not same");
+//		int i = 1;
+//		switch(i){
+//			case 1:
+//				System.out.println("case 1");
+//				
+//				System.out.println("case 1 2nd line");
+//				
+//				
+//				
+//			case 2:System.out.println("case 2");
+//			
+//			default:System.out.println("default");
+//		}
 	}
 	
 	
